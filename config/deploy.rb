9db479d3511b35509871f0de34388d24497711af
Darren Coxall <darren@darrencoxall.com>
2013-01-27 15:39:44 +0000

    Allow sitemaps to be shared between releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,13 +13,19 @@ set :deploy_via, :remote_cache
 
 server '176.56.62.140', :web, :app, :db, primary: true
 
-after 'deploy:update_code', 'deploy:symlink_config'
 after 'deploy:update_code', 'deploy:prevent_htaccess_overwrite'
+after 'deploy:symlink', 'deploy:symlink_config'
 after 'deploy:symlink', 'deploy:symlink_uploads'
+after 'deploy:update_code', 'deploy:symlink_sitemap'
 
 namespace :deploy do
   task :symlink_uploads, roles: :app do
     run "ln -nfs #{shared_path}/uploads #{release_path}/wp-content/uploads"
+  end
+
+  task :symlink_sitemap, roles: :app do
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml #{release_path}/sitemap.xml"
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml.gz #{release_path}/sitemap.xml.gz"
   end
 
   task :symlink_config, roles: :app do
